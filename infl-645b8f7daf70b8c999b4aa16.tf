5d850876b183df4bd0a62ed91fa9f37a1f65a53c
Liav Yona <liav@infralight.co>
2023-05-10 15:35:10 +0300

    Codification: aws_ssm_parameter

---
+++ infl-645b8f7daf70b8c999b4aa16.tf
@@ -0,0 +1,6 @@
+resource "aws_ssm_parameter" "prod-flywheel-fetcher-producer-chunks-138" {
+  name  = "prod-flywheel-fetcher-producer-chunks"
+  type  = "String"
+  value = "REDACTED-BY-FIREFLY:6f4b6612125fb3a0daecd2799dfd6c9c299424fd920f9b308110a2c1fbd8f443:sha256"
+}
+
