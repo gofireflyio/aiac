ee381be94e9fd6ab99fb4d24f3831377b55ee650
Rado Marina <rado@gofirefly.io>
2023-06-14 11:18:46 +0300

    Codification: aws_ssm_parameter

---
+++ infl-648977e54a86ea8bae40c0ec.tf
@@ -0,0 +1,6 @@
+resource "aws_ssm_parameter" "dev-a-notifier-lock-9bd" {
+  name  = "dev-a-notifier-lock"
+  type  = "String"
+  value = "REDACTED-BY-FIREFLY:fcbcf165908dd18a9e49f7ff27810176db8e9f63b4352213741664245224f8aa:sha256"
+}
+
