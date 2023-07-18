e0d4fa4fbd12cfec3bded3a03de4a6232963376a
Liav Yona <liav@infralight.co>
2023-07-18 16:35:52 +0300

    Codification: aws_ssm_parameter

---
+++ provider.tf
@@ -0,0 +1,15 @@
+# The code was generated for this provider version (it can be changed to your preference). 
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.8.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
